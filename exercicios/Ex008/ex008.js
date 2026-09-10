9eddd14d5b6e8af879f33b254513523f94b991fb
helderneiva <helderneiva2002@gmail.com>
2026-09-10 03:29:01 -0300

    ex010

---
+++ exercicios/Ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 130
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 120){
+    console.log('Você ultrapassou o limite de velocidade permitido. MULTADO!')
+}
+console.log('Dirija sempre com cuidado!')
---
+++ exercicios/Ex009/ex009.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Vivendo nos ${país}`)
+if(país == 'Brasil'){
+    console.log('brasileiro!')
+}
+else {
+    console.log('estrangeiro!')
+}
