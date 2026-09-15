d74d01e44b87b5f0c51f69836e09118593afcd4a
helderneiva <helderneiva2002@gmail.com>
2026-09-15 10:48:00 -0300

    auttn14

--- exercicios/Ex015/script.js
+++ exercicios/Ex015/script.js
@@ -4,16 +4,18 @@ function verificar(){
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
     var anoDigitado = Number(fano.value)
-    if (fano.value.length == 0 || anoDigitado <= 0 || anoDigitado > ano) {
-        window.alert(`[ERRO] Verifique os dados e tente novamente!`)
+    if (anoDigitado.length == 0 || anoDigitado <= 0 || anoDigitado > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - anoDigitado
         var gênero = ''
+    
         if (fsex[0].checked) {
             gênero = 'Homem'
             if (idade >= 0 && idade < 2) {
                 //bebe
+        
             } else if (idade < 11) {
                 //criança
             } else if (idade < 19) {
